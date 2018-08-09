1c341e8fcf674859f2403513e7d09d5af4c0a465
Allison Lemmer <alemmer1@jhu.edu>
2018-08-09 02:59:27 -0400

    Streamlining code for mus16s samples

--- align_bwa_mus16s.sh
+++ align_bwa_mus16s.sh
@@ -8,11 +8,13 @@ fi
 
 if [ "$1" == "align" ] ;
 then
-
+	samples='36 37 38 44 45 46'
 	dir=/dilithium/Data/Nanopore/Analysis/alemmer/mus16s_samples
-	fq=sample.38.202
 	ref=./16SMicrobial_removedNewLines.fasta
-
-	bwa mem -x ont2d ${ref} ${dir}/${fq}.fq.gz > bwamem_${fq}.sam
+	
+	for sample in $samples
+	do
+		bwa mem -x ont2d ${ref} ${dir}/sample.${sample}.202.fq.gz > bwamem_sample.${sample}.202.sam
+	done
 
 fi
